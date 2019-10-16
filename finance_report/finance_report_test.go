27acdf09ea3ea431f057ea635642d7e8addec4cf
Aaron Chang <xesuschang@gmail.com>
2019-10-16 11:54:41 +0800

    add income statement, balance sheet data

--- finance_report/finance_report_test.go
+++ finance_report/finance_report_test.go
@@ -7,38 +7,100 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var fixture financeReport.IncomeStatementData
+var fixtureIS financeReport.IncomeStatementData
+var fixtureBS financeReport.BalanceSheetData
 
 func init() {
-	fixture = financeReport.IncomeStatementData{
-		ICostOfGoodsSold:                       75171000.0,
-		IEPS:                                   2.32,
-		IEXDF:                                  -12000.0,
-		IFinancialCost:                         98000.0,
-		IGrossProfit:                           251528000.0,
-		IIncomeAfterTaxes:                      91233000.0,
-		IManagementExp:                         31916000.0,
-		INonControllingTotalConsolidatedProfit: 7609000.0,
-		IOperatingExpenses:                     135036000.0,
-		IOperatingIncome:                       116492000.0,
-		IOtherComprehensiveIncome:              -10000.0,
-		IOtherNonOperatingRevenue:              1543000.0,
-		IOTHNOE:                                -4782000.0,
-		IParentCompanyTotalConsolidatedProfit:  83614000.0,
-		IPreTaxIncome:                          113155000.0,
-		IRDExp:                                 17377000.0,
-		IRevenue:                               326699000.0,
-		ISellingExp:                            85616000.0,
-		ITAX:                                   21922000.0,
-		ITotalConsolidatedProfitForThePeriod:   91223000.0,
-		ITotalNonoperatingIncomeAndExpense:     -3337000.0,
+	fixtureIS = financeReport.IncomeStatementData{
+		CostOfGoodsSold:                       75171000.0,
+		EPS:                                   2.32,
+		EXDF:                                  -12000.0,
+		FinancialCost:                         98000.0,
+		GrossProfit:                           251528000.0,
+		IncomeAfterTaxes:                      91233000.0,
+		ManagementExp:                         31916000.0,
+		NonControllingTotalConsolidatedProfit: 7609000.0,
+		OperatingExpenses:                     135036000.0,
+		OperatingIncome:                       116492000.0,
+		OtherComprehensiveIncome:              -10000.0,
+		OtherNonOperatingRevenue:              1543000.0,
+		OTHNOE:                                -4782000.0,
+		ParentCompanyTotalConsolidatedProfit:  83614000.0,
+		PreTaxIncome:                          113155000.0,
+		RDExp:                                 17377000.0,
+		Revenue:                               326699000.0,
+		SellingExp:                            85616000.0,
+		TAX:                                   21922000.0,
+		TotalConsolidatedProfitForThePeriod:   91223000.0,
+		TotalNonoperatingIncomeAndExpense:     -3337000.0,
+	}
+
+	fixtureBS = financeReport.BalanceSheetData{
+		AccountsPayable:                           3654000,
+		AccountsReceivableNet:                     88719000,
+		AccountsReceivableNet_per:                 6,
+		CapitalStock:                              300175008,
+		CapitalStock_per:                          21,
+		CapitalSurplus:                            348272992,
+		CapitalSurplus_per:                        24,
+		CapitalSurplusAdditionalPaidInCapital:     348135008,
+		CapitalSurplusAdditionalPaidInCapital_per: 24,
+		CashAndCashEquivalents:                    925836992,
+		CashAndCashEquivalents_per:                65,
+		CurrentAssets:                             1246294016,
+		CurrentAssets_per:                         87,
+		CurrentLiabilities:                        367108000,
+		CurrentLiabilities_per:                    26,
+		DeferredTaxAssets:                         11090000,
+		DeferredTaxAssets_per:                     1,
+		Equity:                                    1034286976,
+		Equity_per:                                72,
+		EquityAttributableToOwnersOfParent:        977667968,
+		EquityAttributableToOwnersOfParent_per:    68,
+		IntangibleAssets:                          12018000,
+		IntangibleAssets_per:                      1,
+		Inventories:                               25431000,
+		Inventories_per:                           2,
+		LegalReserve:                              11508000,
+		LegalReserve_per:                          1,
+		Liabilities:                               394116992,
+		Liabilities_per:                           28,
+		LongtermBorrowings:                        18057000,
+		LongtermBorrowings_per:                    1,
+		NoncontrollingInterests:                   56619000,
+		NoncontrollingInterests_per:               4,
+		NoncurrentAssets:                          182110000,
+		NoncurrentAssets_per:                      13,
+		NoncurrentLiabilities:                     27009000,
+		NoncurrentLiabilities_per:                 2,
+		OrdinaryShare:                             300175008,
+		OrdinaryShare_per:                         21,
+		OtherCurrentAssets:                        33539000,
+		OtherCurrentAssets_per:                    2,
+		OtherCurrentLiabilities:                   6536000,
+		OtherEquityInterest:                       -17000,
+		OtherNoncurrentAssets:                     73614000,
+		OtherNoncurrentAssets_per:                 5,
+		OtherNoncurrentLiabilities:                5862000,
+		OtherPayables:                             90678000,
+		OtherPayables_per:                         5986000,
+		OtherReceivable_per:                       85388000,
+		PropertyPlantAndEquipment:                 6,
+		PropertyPlantAndEquipment_per:             329236992,
+		RetainedEarnings:                          23,
+		RetainedEarnings_per:                      317728992,
+		UnappropriatedRetainedEarningsAaccumulatedDeficit:     22,
+		UnappropriatedRetainedEarningsAaccumulatedDeficit_per: 3126000,
 	}
 }
 
-func TestReporterIncomeStatement(t *testing.T) {
-	r := financeReport.New(fixture)
+func TestNew(t *testing.T) {
+	r := financeReport.New(fixtureIS, fixtureBS)
 
-	assert.Equal(t, 75171000.0, r.CostOfGoodsSold(), "wrong cogs")
-	assert.Equal(t, 2.32, r.EPS(), "wrong eps")
-	assert.Equal(t, 21922000.0, r.TAX(), "wrong tax")
+	assert.Equal(t, 75171000.0, r.CostOfGoodsSold, "wrong IS")
+	assert.Equal(t, 2.32, r.EPS, "wrong IS")
+	assert.Equal(t, 17377000.0, r.RDExp, "wrong IS")
+	assert.Equal(t, 21922000.0, r.TAX, "wrong IS")
+	assert.Equal(t, 90678000, r.OtherPayables, "wrong BS")
+	assert.Equal(t, 23, r.RetainedEarnings, "wrong BS")
 }
--- finance_report/report.go
+++ finance_report/report.go
@@ -1,33 +1,8 @@
 package financeReport
 
-type ReportData struct {
-	IncomeStatementData
-}
-
-func New(is IncomeStatementData) Reporter {
+func New(is IncomeStatementData, bs BalanceSheetData) ReportData {
 	return ReportData{
-		IncomeStatementData{
-			ICostOfGoodsSold:                       is.ICostOfGoodsSold,
-			IEPS:                                   is.IEPS,
-			IEXDF:                                  is.IEXDF,
-			IFinancialCost:                         is.IFinancialCost,
-			IGrossProfit:                           is.IGrossProfit,
-			IIncomeAfterTaxes:                      is.IIncomeAfterTaxes,
-			IManagementExp:                         is.IManagementExp,
-			INonControllingTotalConsolidatedProfit: is.INonControllingTotalConsolidatedProfit,
-			IOperatingExpenses:                     is.IOperatingExpenses,
-			IOperatingIncome:                       is.IOperatingIncome,
-			IOtherComprehensiveIncome:              is.IOtherComprehensiveIncome,
-			IOtherNonOperatingRevenue:              is.IOtherNonOperatingRevenue,
-			IOTHNOE:                                is.IOTHNOE,
-			IParentCompanyTotalConsolidatedProfit:  is.IParentCompanyTotalConsolidatedProfit,
-			IPreTaxIncome:                          is.IPreTaxIncome,
-			IRDExp:                                 is.IRDExp,
-			IRevenue:                               is.IRevenue,
-			ISellingExp:                            is.ISellingExp,
-			ITAX:                                   is.ITAX,
-			ITotalConsolidatedProfitForThePeriod:   is.ITotalConsolidatedProfitForThePeriod,
-			ITotalNonoperatingIncomeAndExpense:     is.ITotalNonoperatingIncomeAndExpense,
-		},
+		IncomeStatementData: is,
+		BalanceSheetData:    bs,
 	}
 }
